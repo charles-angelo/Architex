fb24ecb43e45b5e791c2eac0ade6495a12a49d0d
Yawner23 <keefepatosa8@gmail.com>
2026-06-15 21:32:44 +0800

    dwa

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -25,7 +25,7 @@
 
                 <div
                     class="absolute text-9xl top-[8rem] right-10 font-bold text-white select-none -mt-10 outlined-number transition-colors duration-300 group-hover:text-[#1E4D2B]">
-                    011111111111111111
+                    01111111111111111111111
                 </div>
 
                 <div
